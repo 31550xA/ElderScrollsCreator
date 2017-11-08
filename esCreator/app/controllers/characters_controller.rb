3bfd7c252229aa3714bff42533b656a202230f2f
Christopher Bare <christopherbare@outlook.com>
2017-11-08 16:19:25 -0500

    Fixed issue with the index page. Missing end statment in appliaction.html.erb

--- esCreator/app/controllers/characters_controller.rb
+++ esCreator/app/controllers/characters_controller.rb
@@ -2,42 +2,43 @@ class CharactersController < ApplicationController
     def new
         @character = Character.new
     end
-    
+
     def index
         @characters = Character.all
     end
-    
+
     def show
         @character = Character.find(params[:id])
     end
-    
+
     def edit
         @character = Character.find(params[:id])
     end
-    
+
     def update
         @character = Character.find(params[:id])
         if @character.update(character_params)
             redirect_to @character
-            
+
         else
             render 'edit'
         end
     end
-    
+
+
     def destroy
         @character = Character.find(params[:id])
         @character.destroy
-        
+
         redirect_to character_path
     end
-    
+
     def create
         @character = Character.new(character_params)
-        
-        
+
+
         if @character.save
-            
+
             redirect_to @character
         else
             render 'new'
